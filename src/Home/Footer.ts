48bfc01ab1d5f1e86bbf106e658dfa2998ed4c81
Ahmed Salama Ahmed <43586099+salama135@users.noreply.github.com>
2021-12-20 20:16:21 +0200

    Update Footer.ts

--- src/Home/Footer.ts
+++ src/Home/Footer.ts
@@ -44,7 +44,7 @@ export class Footer extends LitElement {
     `
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <footer>
+    <footer class="border-top border-5 border-primary">
         <div class="container pt-5 border-bottom">
             <div class="row">
                 <div class="col-md-3 col-sm-6 col-6 mb-3 p-0 float-left">
