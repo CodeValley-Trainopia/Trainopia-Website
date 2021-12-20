8fcf07cada7b2035e11b2d5ca01ed4190de020fc
Ahmed Salama Ahmed <43586099+salama135@users.noreply.github.com>
2021-12-20 19:32:54 +0200

    Update LocalizationLookUp.ts

--- src/LocalizationLookUp.ts
+++ src/LocalizationLookUp.ts
@@ -1,8 +1,79 @@
 export class LookUp {
     static LookUpTable = {
-        title: {
+        menu_title: {
             ar: "ترانوبيا",
             en: "Trainopia"
-        }
+        },
+        menu_home: {
+            ar: "الرئيسية",
+            en: "Home"
+        },
+        menu_about: {
+            ar: "من نحن",
+            en: "About Us"
+        },
+        menu_contact: {
+            ar: "تواصل معنا",
+            en: "Contact Us"
+        },
+        menu_events: {
+            ar: "فعاليات",
+            en: "Events"
+        },
+        jumbotron_title: {
+            ar: "مرحبا في ترينوبيا",
+            en: "Welcome To Trainopia"
+        },
+        jumbotron_text: {
+            ar: "ترينوبيا جزيرة التغير",
+            en: "The Island Of Change"
+        },
+        jumbotron_button: {
+            ar: "اعرف اكثر",
+            en: "See More!"
+        },
+        howitworks_title: {
+            ar: "كيف يتعلم الطلاب",
+            en: "How It Works"
+        },
+        howitworks_card_header_1: {
+            ar: "اعرف اكثر",
+            en: "howitworks_card_header_1"
+        },
+        howitworks_card_title_1: {
+            ar: "اعرف اكثر",
+            en: "howitworks_card_title_1"
+        },
+        howitworks_card_text_1: {
+            ar: "اعرف اكثر",
+            en: "howitworks_card_text_1"
+        },
+        howitworks_card_header_2: {
+            ar: "اعرف اكثر",
+            en: "howitworks_card_header_2"
+        },
+        howitworks_card_title_2: {
+            ar: "اعرف اكثر",
+            en: "howitworks_card_title_2"
+        },
+        howitworks_card_text_2: {
+            ar: "اعرف اكثر",
+            en: "howitworks_card_text_3"
+        },
+        howitworks_card_header_3: {
+            ar: "اعرف اكثر",
+            en: "howitworks_card_header_3"
+        },
+        howitworks_card_title_3: {
+            ar: "اعرف اكثر",
+            en: "howitworks_card_title_3"
+        },
+        howitworks_card_text_3: {
+            ar: "اعرف اكثر",
+            en: "howitworks_card_text_3"
+        },
+        
+
+
     }
 }
