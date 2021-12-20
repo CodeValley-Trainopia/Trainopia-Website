6516d5411f4af962d3834ca4935c363941105234
Ahmed Salama Ahmed <43586099+salama135@users.noreply.github.com>
2021-12-20 19:38:38 +0200

    Update Footer.ts

--- src/Home/Footer.ts
+++ src/Home/Footer.ts
@@ -47,21 +47,6 @@ export class Footer extends LitElement {
     <footer>
         <div class="container pt-5 border-bottom">
             <div class="row">
-                <div class="col-md-3 col-sm-6 col-6 p-0 float-left mb-3">
-                    <h5 class="mb-4 font-weight-bold text-uppercase">Solutions</h5>
-                    <ul class="list-group">
-
-                    </ul>
-                </div>
-
-                <div class="col-md-3 col-sm-6 col-6 p-0 mb-3 float-left">
-                    <h5 class="mb-4 font-weight-bold text-uppercase">Developers</h5>
-                    <ul class="list-group">
-                        <li class="list-group-item bg-transparent border-0 p-0 mb-2"><a href="https://github.com/naologic">Open Source</a></li>
-                        <li class="list-group-item bg-transparent border-0 p-0 mb-2"><a href="https://stackshare.io/naologic">Technology</a></li>
-                    </ul>
-                </div>
-
                 <div class="col-md-3 col-sm-6 col-6 mb-3 p-0 float-left">
                     <h5 class="mb-4 font-weight-bold text-uppercase">Company</h5>
                     <ul class="list-group">
@@ -69,7 +54,7 @@ export class Footer extends LitElement {
                             <a href="../about-naologic.html"></i>About</a>
                         </li>
                         <li class="list-group-item bg-transparent border-0 p-0 mb-2">
-                            <a href="https://blog.naologic.com"></i> Blog</a>
+                            <a href="https://blog.naologic.com"></i>Blog</a>
                         </li>
                     </ul>
                 </div>
@@ -77,25 +62,6 @@ export class Footer extends LitElement {
                 <div class="col-md-3 col-sm-6 col-6 mb-3 p-0 float-left">
                     <h5 class="mb-4 font-weight-bold text-uppercase">Connect</h5>
                     <ul class="list-group">
-                        <li class="list-group-item bg-transparent border-0 p-0 mb-2">
-                            <a href="https://www.linkedin.com/company/naologic"><i class="fa fa-linkedin mr-1"></i> LinkedIn</a>
-                        </li>
-                        <li class="list-group-item bg-transparent border-0 p-0 mb-2">
-                            <a href="https://twitter.com/naologicerp"><i class="fa fa-twitter mr-1"></i> Twitter</a>
-                        </li class="list-group-item bg-transparent border-0 p-0 mb-2">
-                        <li class="list-group-item bg-transparent border-0 p-0 mb-2">
-                            <a href="https://www.reddit.com/r/naologic/"><i class="fa fa-reddit mr-1"></i> Reddit</a>
-                        </li>
-                        <li class="list-group-item bg-transparent border-0 p-0 mb-2">
-                            <a href="https://plus.google.com/109511516185666043480" target="_blank"><i class="fa fa-google-plus mr-1"></i> Google+</a>
-
-                        </li>
-                        <li class="list-group-item bg-transparent border-0 p-0 mb-2">
-                            <a href="https://github.com/naologic" target="_blank"><i class="fa fa-github mr-1"></i> Github</a>
-                        </li>
-                        <li class="list-group-item bg-transparent border-0 p-0 mb-2">
-                            <a href="https://blog.naologic.com" target="_blank"><i class="fa fa-medium mr-1"></i> Medium</a>
-                        </li>
                         <li class="list-group-item bg-transparent border-0 p-0 mb-2">
                             <a href="https://www.facebook.com/naologic/" target="_blank"><i class="fa fa-facebook mr-1"></i> Facebook</a>
                         </li>
@@ -108,8 +74,7 @@ export class Footer extends LitElement {
                 <div class="col-md-12">
                 <div class="py-4 d-flex justify-content-center">
                     <div class="d-flex flex-column bd-highlight mb-3">
-                        <div class="p-2 bd-highlight"><a class="" href="../privacy.html">Privacy & terms</a></div>
-                        <div class="p-2 bd-highlight"><a class="" href="../sitemap.xml">Sitemap</a></div>
+                        <div class="p-2 bd-highlight">CodeValley @ 2021</div>
                     </div>    
                     </div>
                 </div>
