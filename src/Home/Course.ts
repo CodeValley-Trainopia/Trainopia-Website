7e7697e3c91be28762cae45efa4631c116525a47
Ahmed Salama Ahmed <43586099+salama135@users.noreply.github.com>
2021-12-20 20:08:07 +0200

    Update Course.ts

--- src/Home/Course.ts
+++ src/Home/Course.ts
@@ -1,11 +1,22 @@
 import { LitElement, html, css } from 'lit';
+import { property } from 'lit-element';
 
 export class Course extends LitElement {
-    color: unknown;
+    IsRTL: unknown;
+    Color: unknown;
+    Title: unknown;
+    Text: unknown;
+    ButtonRight_Text: unknown;
+    ButtonLeft_Text: unknown;
 
     static get properties() {
         return {
-            color: { type: String }
+            Color: { type: String },
+            IsRTL: { type: String },
+            Title: { type: String },
+            Text: { type: String },
+            ButtonRight_Text: { type: String },
+            ButtonLeft_Text: { type: String }
         };
     }
 
@@ -22,25 +33,25 @@ export class Course extends LitElement {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 
-        <div class="card mb-3 border-0">
-            <div class="row ">
+        <div class="card mb-3 border-0" dir=${(this.IsRTL === "true")? "rtl": "ltr"}>
+            <div class="row ">                
                 <div class="col-md-6 col-sm-12">
-                    <div class="card bg-${this.color} shadow bg-gradient border-5 border-top-0 border-start-0 border-end-0 border-secondary h-100">
-                    </div>
-                </div>
-                <div class="col-md-6 col-sm-12 ">
-                    <div class="card-body border-5">
-                        <h5 class="card-title fw-thin display-1 text-${this.color}">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="card-body border-5"  dir=${(this.IsRTL === "true")? "rtl": "ltr"}>
+                        <h5 class="card-title fw-thin display-1 text-${this.Color}">${this.Title}</h5>
+                        <p class="card-text">${this.Text}</p>
                         <div class="row g-4">
                             <div class="col-lg-6">
-                                <button type="button" class="btn btn-${this.color} shadow w-100">Primary</button>
+                                <button type="button" class="btn btn-${this.Color} shadow w-100">${this.ButtonLeft_Text}</button>
                             </div>
 
                             <div class="col-lg-6">
-                                <button type="button" class="btn btn-outline-${this.color} shadow w-100">Primary</button>
+                                <button type="button" class="btn btn-outline-${this.Color} shadow w-100">${this.ButtonRight_Text}</button>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <div class="card bg-${this.Color} shadow bg-gradient border-5 border-top-0 border-start-0 border-end-0 border-secondary h-100">
                     </div>
                 </div>
             </div>
