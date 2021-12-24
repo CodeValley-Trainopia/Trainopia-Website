d20814175a359f17f1e87b76f49528461dc32665
Ahmed Salama Ahmed <43586099+salama135@users.noreply.github.com>
2021-12-24 22:55:42 +0200

    Update Index.ts

--- src/Home/Index.ts
+++ src/Home/Index.ts
@@ -1,9 +1,10 @@
 import { LitElement, html, css } from 'lit-element';
-import { LookUp } from '../LocalizationLookUp';
-import { property } from 'lit/decorators.js';
+import { property } from 'lit-element';
 
 
 export class Index extends LitElement {
+
+    @property({type: String}) IsRTL: any;
 
     static get properties() {
         return {
@@ -29,8 +30,32 @@ export class Index extends LitElement {
     render() {
         return html
         `
-        <navbar-comp></navbar-comp>
+        <navbar-comp @click=${this.ToggleRTL} IsRTL=${this.IsRTL}></navbar-comp>
         
+        <br>
+        
+        <jumbotron-comp IsRTL=${this.IsRTL}></jumbotron-comp>
+    
+        <br>
+    
+        <howitworks-comp IsRTL=${this.IsRTL}></howitworks-comp>
+    
+        <br>
+    
+        <courses-comp IsRTL=${this.IsRTL}></courses-comp>
+    
+        <br>
+    
+        <testimonial-comp IsRTL=${this.IsRTL}></testimonial-comp>
+    
+        <br>
+    
+        <footer-comp IsRTL=${this.IsRTL}></footer-comp>
         `;
+    }
+
+    ToggleRTL() {
+        console.log( this.renderRoot.querySelector('navbar-comp')?.getAttribute("RTL"));
+        this.IsRTL = this.renderRoot.querySelector('navbar-comp')?.getAttribute("isrtl");
     }
 }
