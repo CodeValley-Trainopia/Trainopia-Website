5b12d73724ccdac1fd9fe2dffee0d9c9adbb7b29
Ahmed Salama Ahmed <43586099+salama135@users.noreply.github.com>
2022-01-13 01:01:10 +0200

    Create FlipCard.ts

---
+++ src/Home/FlipCard.ts
@@ -0,0 +1,74 @@
+import { LitElement, html, css } from 'lit';
+import { property } from 'lit-element';
+
+export class FlipCard extends LitElement {
+    
+    @property({type: String}) IsRTL: any;
+
+
+    // createRenderRoot() {
+    //     return this;
+    // }
+
+    static get styles() {
+        return css
+        `
+        
+        `;
+    }
+
+    render() {
+        return html
+        `
+        <style>
+        .scene {
+          width: 260px;
+          height: 260px;
+        }
+        
+        .FlipCard {
+          width: 100%;
+          height: 100%;
+          transition: transform 1s;
+          transform-style: preserve-3d;
+          cursor: pointer;
+        }
+        
+        .FlipCard.is-flipped { 
+          background:initial;         
+          transform: rotateY(180deg);
+        }
+        
+        .FlipCard__face {
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          color: white;
+          -webkit-backface-visibility: hidden;
+          backface-visibility: hidden;
+        }
+        
+        .FlipCard__face--front {
+          background:initial;
+        }
+        
+        .FlipCard__face--back {
+          background:initial;
+          transform: rotateY(180deg);
+        }   
+        </style>
+        <div class="scene scene--FlipCard">
+            <div class="FlipCard" @click=${this.toggle}>
+                <div class="FlipCard__face FlipCard__face--front"><slot name="front"></slot></div>
+                <div class="FlipCard__face FlipCard__face--back"><slot name="back"></slot></div>
+            </div>
+        </div>
+        `;
+    }
+
+    toggle(){
+        var FlipCard = this.renderRoot.querySelector('.FlipCard');        
+        FlipCard?.classList.toggle('is-flipped');
+    }
+
+}
