f0562a96c15599f884f83d0b151dac96e4b130fa
Ahmed Salama Ahmed <43586099+salama135@users.noreply.github.com>
2022-01-12 20:51:48 +0200

    Update Navbar.ts

--- src/Shared/Navbar.ts
+++ src/Shared/Navbar.ts
@@ -56,9 +56,13 @@ export class Navbar extends LitElement {
                 border: transparent;
             }
         }
+        nav {
+            background: #ebf4fa;
+        }
         </style>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-        <nav dir=${(this.IsRTL === "true")? "rtl": "ltr"} style="background: #ebf4fa;" class="navbar fixed-top navbar-expand-lg fs-6 fw-bold border-bottom border-5 border-primary">
+        <nav dir=${(this.IsRTL === "true")? "rtl": "ltr"}  class="navbar fixed-top navbar-expand-lg fs-6 fw-bold border-bottom border-5 border-primary">
             <div class="container-fluid">
                 
                 <span class="material-icons fs-1 text-muted" >
